cd2330b57fdc6bd7d376f173db5199b7b40357f0
Taufiq Ansari <taufiq596@gmail.com>
2019-11-03 17:32:28 +0530

    new update

--- main.js
+++ main.js
@@ -1,13 +1,84 @@
-window.onscroll = ()=>{
-    var pageTop = window.pageYOffset;
-    var navTop = document.querySelector('#navTop').offsetTop;
-    console.log(pageTop, navTop);
-    if(pageTop>navTop){
-        document.querySelector('#navTop').style.postion = 'fixed';
-        document.querySelector('#navTop').style.top = '0';
-        document.querySelector('#navTop').style.zIndex = '1';
+// window.onscroll = ()=>{
+//     // console.log('test')
+//     var pageTop = window.pageYOffset;
+//     var navTop = document.querySelector('#navTop').offsetTop;
+//     console.log(pageTop, navTop);
+//     if(pageTop>navTop){
+//         console.log('test')
+//         document.querySelector('#navTop').style.position = 'fixed';
+//         document.querySelector('#navTop').style.top = '0';
+//         document.querySelector('#navTop').style.zIndex = '1';
+
+//     }
+//     else{
+//         document.querySelector('#navTop').style.position = 'static';
+//     }
+
+//     var containers = document.querySelectorAll(".scroll-active");
+//     var navItems = document.querySelectorAll(".nav-item");
+//     containers.forEach((item,index) => {
+//         if(window.pageYOffset > item.offsetTop){
+//             navItems.forEach((values, key)=>{
+//                 if(index == key){
+//                     values.className += " active"
+//                 }
+//                 else{
+//                     values.className = "nav-item"
+//                 }
+//             })
+//         }
+//     })
+// }
+window.onscroll = function(){
+    var navbar = document.querySelector(".navbar");
+    if(window.pageYOffset > 0){
+        navbar.style.position = 'fixed';
+        navbar.style.zIndex = 100;
+        navbar.style.width = '100%';
     }
     else{
-        document.querySelector('#navTop').style.position = 'static';
+        navbar.style.position = 'static';
     }
+    var containers = document.querySelectorAll(".scroll-active");
+    var navItems = document.querySelectorAll(".nav-item");
+    containers.forEach((item,index) => {
+        if(window.pageYOffset > item.offsetTop){
+            navItems.forEach((values, key)=>{
+                if(index == key){
+                    values.className += " active"
+                }
+                else{
+                    values.className = "nav-item"
+                }
+            })
+        }
+    })
 }
+
+// window.onscroll=function(){
+//     if(window.pageYOffset>0){
+//         document.querySelector('.navbar').style.position = 'fixed';
+//         document.querySelector('.navbar').style.top = '0';
+//         document.querySelector('.navbar').style.zIndex = '1';
+//         document.querySelector('.navbar').style.width  = '100%';
+//     }
+//     else{
+//         document.querySelector('.navbar').style.position = 'static';
+//     }
+//     var containers = document.querySelectorAll(".scroll-active");
+//     var navItems = document.querySelectorAll(".nav-item");
+//     containers.forEach((item,index) => {
+//         if(window.pageYOffset > item.offsetTop){
+//             navItems.forEach((values, key)=>{
+//                 if(index==key){
+//                     values.className += " active"
+                    
+//                 }
+//                 else{
+//                     values.className = 'nav-item'
+//                 }
+//             })
+//         }
+//     })
+
+// 
